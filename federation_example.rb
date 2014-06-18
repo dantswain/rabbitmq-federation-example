37b891332462b7298e3dd3a3089474f9dadfe3dd
Dan Swain <dan.t.swain@gmail.com>
2014-06-17 19:45:23 -0500

    Example using multiple pubs/subs

--- federation_example.rb
+++ federation_example.rb
@@ -26,15 +26,32 @@ nodes[1].channel.topic('federated_1')
 t5 = nodes[5].channel.topic('federated_1')
 q5 = nodes[5].channel.queue('node5_fed_listener', durable: true)
 q5.bind(t5, routing_key: 'test.*')
+t2 = nodes[2].channel.topic('federated_1')
+q2 = nodes[2].channel.queue('node2_fed_listener', durable: true)
+q2.bind(t2, routing_key: 'test.*')
 
 # publish a message on node 2
 puts 'PUBLISHING'
-from_node = 4
-nodes[from_node].channel.topic('federated_1').publish("hi test from node #{from_node}", routing_key: 'test.foo')
+(1..5).each do |from_node|
+  x = nodes[from_node].channel.topic('federated_1')
+  x.on_return do |info, prop, payload|
+    puts "UH OH #{info.inspect}, #{prop.inspect}, #{payload.inspect}"
+  end
+  x.publish("hi test from node #{from_node}", routing_key: 'test.foo')
+end
 
-q5.subscribe(block: true) do |di, m, p|
-  puts "GOT #{p.inspect}"
-  di.consumer.cancel
+puts 'WAITING'
+sleep(0.5)
+loop do
+  _di, _m, p = q5.get
+  break unless p
+  puts "GOT (node 5) #{p.inspect}"
+end
+
+loop do
+  _di, _m, p = q2.get
+  break unless p
+  puts "GOT (node 2) #{p.inspect}"
 end
 
 puts 'DONE'

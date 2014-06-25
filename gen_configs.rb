1a42603dab8e8108bd29af17d6de554dfeaff96d
Dan Swain <dan.t.swain@gmail.com>
2014-06-25 13:20:54 -0500

    Restore star topology

--- gen_configs.rb
+++ gen_configs.rb
@@ -28,11 +28,11 @@ end
 #   node 3 has node 1 as upstream
 #     .. etc
 topology = {
-  1 => [3, 4],
-  2 => [3, 4],
-  3 => [1, 2, 4, 5],
-  4 => [1, 2, 3, 5],
-  5 => [3, 4]
+  1 => [2, 3, 4, 5],
+  2 => [1],
+  3 => [1],
+  4 => [1],
+  5 => [1]
 }
 
 # another example - a directed ring graph
